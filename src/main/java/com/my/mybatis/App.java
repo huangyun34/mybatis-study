0558ec86f27e5a18305df62a5b363df9842fd03f
黄云 <huangyun34@163.com>
2021-03-12 15:40:51 +0800

    demo

---
+++ src/main/java/com/my/mybatis/App.java
@@ -0,0 +1,12 @@
+package com.my.mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException {
+        System.out.println( "Hello World!" );
+    }
+}
