81a2d3a3195cdf87734f3e86a50acf15eb8df760
黄云 <huangyun34@163.com>
2021-03-12 17:47:12 +0800

    demo

---
+++ src/main/java/com/my/mybatis/dao/AdminUserMapper.java
@@ -0,0 +1,16 @@
+package com.my.mybatis.dao;
+
+import com.my.mybatis.domain.AdminUser;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface AdminUserMapper {
+
+    @Select("select * from land_admin_user where id = #{id}")
+    AdminUser selectById(@Param("id") Long id);
+
+    @Select("select * from land_admin_user where is_delete = #{isDelete}")
+    List<AdminUser> selectByIsDelete(@Param("isDelete") Integer isDelete);
+}
---
+++ src/main/java/com/my/mybatis/package-info.java
@@ -0,0 +1,11 @@
+package com.my.mybatis;
+
+/**
+ * mybatis描述
+ * 要使用mybatis有如下基本步骤
+ * 1。
+ * 2。
+ * 3。
+ * 4。
+ * 5。
+ */
